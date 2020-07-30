bdd4efa217743fddac36064c869607221736f59d
Laszlo Balint <laszlobalint1987@gmail.com>
2020-07-31 00:12:23 +0200

    Init react app

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'React App'));
+  }
+}
+
+export default App;
