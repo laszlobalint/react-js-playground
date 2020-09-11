404120a8eadfabe65019e4ca39d98914cb29e86a
Balint Laszlo <balint.laszlo@t-systems.com>
2020-09-11 09:22:30 +0200

    Input validation

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -4,17 +4,24 @@ import classes from './Input.module.css';
 
 const Input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+  let validationError = null;
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+    validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>;
+  }
 
   switch (props.elementType) {
     case 'input':
-      inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />;
+      inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />;
       break;
     case 'textarea':
-      inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />;
+      inputElement = <textarea className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />;
       break;
     case 'select':
       inputElement = (
-        <select className={classes.InputElement} value={props.value} onChange={props.changed}>
+        <select className={inputClasses.join(' ')} value={props.value} onChange={props.changed}>
           {props.elementConfig.options.map((option) => (
             <option key={option.value} value={option.value}>
               {option.displayValue}
@@ -24,13 +31,14 @@ const Input = (props) => {
       );
       break;
     default:
-      inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+      inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} />;
   }
 
   return (
     <div className={classes.Input}>
       <label className={classes.Label}>{props.label}</label>
       {inputElement}
+      {validationError}
     </div>
   );
 };
--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -16,6 +16,7 @@ class ContactData extends Component {
       country: this.createInputConfig('input', 'text', 'Enter your country...'),
       deliveryMethod: this.createInputConfig('select', null, null, true),
     },
+    formIsValid: false,
     loading: false,
     error: false,
   };
@@ -29,6 +30,13 @@ class ContactData extends Component {
           placeholder,
         },
         value: '',
+        validation: {
+          required: true,
+          minLength: 3,
+          maxLength: 50,
+        },
+        valid: false,
+        touched: false,
       };
     } else {
       return {
@@ -40,6 +48,10 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' },
           ],
         },
+        value: 'fastest',
+        validation: {},
+        valid: true,
+        touched: false,
       };
     }
   }
@@ -66,12 +78,28 @@ class ContactData extends Component {
       });
   };
 
+  checkValidity = (value, rules) => {
+    let isValid = true;
+    if (rules.required) isValid = value.trim() !== '' && isValid;
+    if (rules.minLength) isValid = value.length >= rules.minLength && isValid;
+    if (rules.maxLength) isValid = value.length <= rules.maxLength && isValid;
+
+    return isValid;
+  };
+
   inputChangedHandler = (event, inputId) => {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedFormElement = { ...updatedOrderForm[inputId] };
     updatedFormElement.value = event.target.value;
+
+    updatedFormElement.touched = true;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputId] = updatedFormElement;
-    this.setState({ orderForm: updatedOrderForm });
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+
+    this.setState({ orderForm: updatedOrderForm, formIsValid });
   };
 
   render() {
@@ -86,10 +114,15 @@ class ContactData extends Component {
             elementType={element.config.elementType}
             elementConfig={element.config.elementConfig}
             value={element.config.value}
+            invalid={!element.config.valid}
+            shouldValidate={element.config.validation}
+            touched={element.config.touched}
             changed={(event) => this.inputChangedHandler(event, element.id)}
           />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>
+          ORDER
+        </Button>
       </form>
     );
     if (this.state.loading) form = <Spinner />;
