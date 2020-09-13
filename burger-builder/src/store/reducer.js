32a5af71c78ad5d99c9d1d394be9a88b4fea4679
Laszlo Balint <laszlobalint1987@gmail.com>
2020-09-13 20:10:12 +0200

    Add Redux begin.

---
+++ burger-builder/src/store/reducer.js
@@ -0,0 +1,36 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  },
+  totalPrice: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName]++,
+        },
+      };
+    case actionTypes.REMOVE_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName]--,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
